f462fb3e52f820ea722e477d9847a28cc5c57f6a
Иван Гордийчук <ivan_gordiychuk@hotmail.com>
2019-09-25 18:24:33 +0300

    Add files via upload

---
+++ 21nd.py
@@ -0,0 +1,33 @@
+import requests
+import json 
+import nltk
+from nltk.corpus import stopwords 
+from nltk.tokenize import word_tokenize 
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+import numpy as np
+
+response = requests.get('https://newsapi.org/v2/everything?q=russia&from=2019-08-25&to=2019-09-25&language=en&contry=US&apiKey=6f795d902bc3494ba973f71e6f476cb3')
+jsonText = json.loads(response.text)
+text=""
+for obj in jsonText['articles']:
+	text+=str(obj['content']).lower()
+
+stopWords = set(stopwords.words('english')) 
+wordTokens = word_tokenize(text)
+filteredText = [word for word in wordTokens if not word in stopWords and word.isalpha() and word != 'chars'] 
+
+dataFrequencies = {}
+
+for word in filteredText:
+    dataFrequencies[word] = dataFrequencies.get(word, 0) + 1 
+  
+dataFrequencies50 = dict(sorted(dataFrequencies.items(), key=lambda f:-f[1])[:50])
+
+wordCloud = WordCloud(width=900,height=500, max_words=50, background_color="white", relative_scaling=1,normalize_plurals=False).generate_from_frequencies(dataFrequencies50)
+
+plt.imshow(wordCloud, interpolation='bilinear')
+plt.axis("off")
+plt.show()
+
+
